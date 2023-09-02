bc41c29d0744f9489bc07577c245618a2b9cc366
tglide <26071571+TGlide@users.noreply.github.com>
2023-09-02 20:18:49 +0100

    edit default content

--- src/lib/ui/editor/default-content.ts
+++ src/lib/ui/editor/default-content.ts
@@ -4,7 +4,7 @@ export const defaultEditorContent = {
 		{
 			type: 'heading',
 			attrs: { level: 2 },
-			content: [{ type: 'text', text: 'Introducing Novel' }]
+			content: [{ type: 'text', text: 'Introducing Novel Svelte' }]
 		},
 		{
 			type: 'paragraph',
@@ -15,14 +15,14 @@ export const defaultEditorContent = {
 						{
 							type: 'link',
 							attrs: {
-								href: 'https://github.com/steven-tey/novel',
+								href: 'https://github.com/tglide/novel-svelte',
 								target: '_blank',
 								class:
 									'text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer'
 							}
 						}
 					],
-					text: 'Novel'
+					text: 'Novel Svelte'
 				},
 				{
 					type: 'text',
@@ -59,7 +59,23 @@ export const defaultEditorContent = {
 					],
 					text: 'Vercel AI SDK'
 				},
-				{ type: 'text', text: '.' }
+				{ type: 'text', text: ". Ported From Steven Tey's " },
+				{
+					type: 'text',
+					marks: [
+						{
+							type: 'link',
+							attrs: {
+								href: 'https://github.com/steven-tey/novel',
+								target: '_blank',
+								class:
+									'text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer'
+							}
+						}
+					],
+					text: 'Novel'
+				},
+				{ type: 'text', text: ' project.' }
 			]
 		},
 		{
@@ -70,7 +86,7 @@ export const defaultEditorContent = {
 		{
 			type: 'codeBlock',
 			attrs: { language: null },
-			content: [{ type: 'text', text: 'npm i novel' }]
+			content: [{ type: 'text', text: 'npm i novel-svelte' }]
 		},
 		{
 			type: 'heading',
@@ -83,7 +99,7 @@ export const defaultEditorContent = {
 			content: [
 				{
 					type: 'text',
-					text: 'import { Editor } from "novel";\n\nexport default function App() {\n  return (\n     <Editor />\n  )\n}'
+					text: '<script>\n\timport { Editor } from "novel-svelte";\n</script>\n\n<Editor />'
 				}
 			]
 		},
@@ -170,7 +186,7 @@ export const defaultEditorContent = {
 										{
 											type: 'link',
 											attrs: {
-												href: 'https://github.com/steven-tey/novel',
+												href: 'https://github.com/tglide/novel-svelte',
 												target: '_blank',
 												class:
 													'text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer'
@@ -197,7 +213,7 @@ export const defaultEditorContent = {
 										{
 											type: 'link',
 											attrs: {
-												href: 'https://www.npmjs.com/package/novel',
+												href: 'https://www.npmjs.com/package/novel-svelte',
 												target: '_blank',
 												class:
 													'text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer'
@@ -206,33 +222,6 @@ export const defaultEditorContent = {
 									],
 									text: 'NPM package'
 								}
-							]
-						}
-					]
-				},
-				{
-					type: 'taskItem',
-					attrs: { checked: false },
-					content: [
-						{
-							type: 'paragraph',
-							content: [
-								{
-									type: 'text',
-									marks: [
-										{
-											type: 'link',
-											attrs: {
-												href: 'https://vercel.com/templates/next.js/novel',
-												target: '_blank',
-												class:
-													'text-stone-400 underline underline-offset-[3px] hover:text-stone-600 transition-colors cursor-pointer'
-											}
-										}
-									],
-									text: 'Deploy your own'
-								},
-								{ type: 'text', text: ' to Vercel' }
 							]
 						}
 					]
